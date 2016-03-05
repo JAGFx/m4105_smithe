89d8f57aa1b6b03d82e766804b8cd7f49e0e5054
JAGFx <contact@jagfx.fr>
2016-03-05 14:31:07 +0100

    Fix thumbnail height

--- m4105_smithe/src/s4smithe/VitrineBundle/Resources/public/sfStore.js
+++ m4105_smithe/src/s4smithe/VitrineBundle/Resources/public/sfStore.js
@@ -9,23 +9,33 @@
 			if (p.child === null)
 				return;
 
-			var obj = $(p.parent + " " + p.child);
+			var obj = $(p.parent + " " + p.child + " .caption");
 			var hElem = (p.toDo === "max") ? 0 : 9999999999999;
 			var action = true;
 			var xsWindow = ($(window).width() < 768);
 			var smWindow = ($(window).width() < 992);
+			var hCurrentElem = 0;
+			
+			/*console.log("INIT -----------------------------");
+			console.log("hElem: " + hElem);
+			console.log("xsWindow: " + xsWindow);
+			console.log("smWindow: " + smWindow);
+			console.log("toDo: " + p.toDo);
+			console.log("END -----------------------------");*/
 
 			obj.each(function () {
 				$(this).removeAttr("style");
+				hCurrentElem = parseInt($(this).outerHeight()) + 182;
 
-				action = (xsWindow || (smWindow && $(this).outerHeight() > 0) || !smWindow);
+				action = (xsWindow || (smWindow && hCurrentElem > 0) || !smWindow);
 
 				if (action && p.toDo === "max")
-					hElem = (parseInt($(this).outerHeight()) >= hElem) ? parseInt($(this).outerHeight()) : hElem;
+					hElem = (hCurrentElem >= hElem) ? hCurrentElem : hElem;
 				else if (action && p.toDo === "min")
-					hElem = (parseInt($(this).outerHeight()) <= hElem) ? parseInt($(this).outerHeight()) : hElem;
+					hElem = (hCurrentElem <= hElem) ? hCurrentElem : hElem;
 				//console.log(action);
-				//console.log(hElem);
+				//console.log("hObj: " + hCurrentElem );
+				//console.log("hElem final: " + hElem);
 			});
 
 			//console.log(!xsWindow);
@@ -39,5 +49,4 @@
 
 		$('#panier').tooltip();
 		$("#contentArticles").setHeightAllElem({child: ".thumbnail"});
-
 	});
